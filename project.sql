b5227b88f67ddf1844b7d2d9c2069284ef5bc904
deepamshree <45068096+deepamshree@users.noreply.github.com>
2023-04-30 19:52:14 +0100

    Update and rename project (4).sql to project.sql

--- project.sql
+++ project.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.3
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 06, 2018 at 11:02 AM
--- Server version: 10.1.36-MariaDB
--- PHP Version: 7.2.11
+-- Generation Time: Apr 30, 2023 at 08:50 PM
+-- Server version: 10.4.25-MariaDB
+-- PHP Version: 8.1.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -26,7 +25,7 @@ DELIMITER $$
 --
 -- Procedures
 --
-CREATE DEFINER=`root`@`localhost` PROCEDURE `getCustomer` ()  SELECT * FROM customer$$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `getCustomer` ()   SELECT * FROM customer$$
 
 DELIMITER ;
 
@@ -48,7 +47,9 @@ CREATE TABLE `cake` (
 --
 
 INSERT INTO `cake` (`eventid`, `weight`, `type`, `flavour`) VALUES
-(1, 89, 'pasteries', 'nmsnx');
+(2, 5, 'pasteries', 'Caramel'),
+(3, 5, 'pasteries', 'cadbury'),
+(4, 2, 'pasteries', 'Choclate');
 
 -- --------------------------------------------------------
 
@@ -69,7 +70,10 @@ CREATE TABLE `customer` (
 --
 
 INSERT INTO `customer` (`eventid`, `name`, `email`, `phone`, `gender`) VALUES
-(1, 'maggi', 'maggi#@gmail.com', 9389392, 'male');
+(2, '<script>alert(\\\"xss\\\")</script>', 'deepa@gmail.com', 101010100, 'female'),
+(3, '9999999999', 'deepa@gmm', 0, 'male'),
+(4, '9999999999', 'deepa@gmm', 11111111111111111, 'female'),
+(5, 'project1', 'deepa@gmm', 123265565, 'male');
 
 --
 -- Triggers `customer`
@@ -98,16 +102,6 @@ CREATE TABLE `decoration` (
   `deconame` varchar(25) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `decoration`
---
-
-INSERT INTO `decoration` (`decoid`, `deconame`) VALUES
-(1, 'Simple Ballon Decoration'),
-(2, 'Helium Ballon Decoration'),
-(3, 'Ballon Arch Decoration'),
-(4, 'Surprize Ballon Decoratio');
-
 -- --------------------------------------------------------
 
 --
@@ -127,7 +121,9 @@ CREATE TABLE `event` (
 --
 
 INSERT INTO `event` (`eventid`, `decoid`, `venue`, `date`, `people`) VALUES
-(1, 1, 'mysire', '2018-12-22', 98);
+(2, 4, 'Dublin 0', '2023-05-03', 55),
+(3, 3, 'Dublin 2', '2023-05-04', 54),
+(4, 1, 'Dublin 1', '2023-05-04', 52);
 
 -- --------------------------------------------------------
 
@@ -146,7 +142,9 @@ CREATE TABLE `food` (
 --
 
 INSERT INTO `food` (`eventid`, `noofpeople`, `types`) VALUES
-(1, 90, 'North-Indian');
+(2, 500, 'North-Indian'),
+(3, 54, 'North-Indian'),
+(4, 0, 'North-Indian');
 
 -- --------------------------------------------------------
 
@@ -167,23 +165,10 @@ CREATE TABLE `logs` (
 --
 
 INSERT INTO `logs` (`sl.no`, `name`, `eventid`, `action`, `c_date`) VALUES
-(1, 'ss', 17, 'inserted', '2018-12-05 23:54:19'),
-(2, 'ss', 17, 'inserted', '2018-12-05 23:56:07'),
-(3, 'dilli', 15, 'UPDATED', '2018-12-05 23:59:06'),
-(4, 'DEEPA', 2, 'DELETED', '2018-12-05 23:59:26'),
-(5, 'nikitha', 6, 'DELETED', '2018-12-05 23:59:32'),
-(6, 'abcd', 7, 'DELETED', '2018-12-05 23:59:46'),
-(7, 'DEEPA', 8, 'DELETED', '2018-12-05 23:59:53'),
-(8, 'dilli', 15, 'DELETED', '2018-12-05 23:59:58'),
-(9, 'ss', 17, 'DELETED', '2018-12-06 00:00:04'),
-(10, 'dee', 18, 'INSERTED', '2018-12-06 00:02:32'),
-(11, 'dee', 19, 'INSERTED', '2018-12-06 00:12:51'),
-(12, 'dee', 18, 'DELETED', '2018-12-06 00:21:44'),
-(13, 'dee', 19, 'DELETED', '2018-12-06 00:21:51'),
-(14, 'maggi', 1, 'INSERTED', '2018-12-06 00:53:55'),
-(15, 'deepa', 2, 'INSERTED', '2018-12-06 14:47:08'),
-(16, 'deepashree', 2, 'UPDATED', '2018-12-06 14:48:39'),
-(17, 'deepashree', 2, 'DELETED', '2018-12-06 14:49:50');
+(19, '<script>alert(\\\"xss\\\")</s', 2, 'INSERTED', '2023-04-30 16:54:30'),
+(20, '9999999999', 3, 'INSERTED', '2023-04-30 17:05:32'),
+(21, '9999999999', 4, 'INSERTED', '2023-04-30 17:08:16'),
+(22, 'project1', 5, 'INSERTED', '2023-04-30 17:10:58');
 
 --
 -- Indexes for dumped tables
@@ -234,13 +219,13 @@ ALTER TABLE `logs`
 -- AUTO_INCREMENT for table `cake`
 --
 ALTER TABLE `cake`
-  MODIFY `eventid` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `eventid` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `customer`
 --
 ALTER TABLE `customer`
-  MODIFY `eventid` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `eventid` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `decoration`
@@ -252,19 +237,19 @@ ALTER TABLE `decoration`
 -- AUTO_INCREMENT for table `event`
 --
 ALTER TABLE `event`
-  MODIFY `eventid` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `eventid` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `food`
 --
 ALTER TABLE `food`
-  MODIFY `eventid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `eventid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `logs`
 --
 ALTER TABLE `logs`
-  MODIFY `sl.no` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `sl.no` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- Constraints for dumped tables
